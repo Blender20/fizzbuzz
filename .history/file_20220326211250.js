89d0d033773fb13718c76850aa7ed5b604bb2079
Blender20 <rayyazdi@outlook.com>
2022-03-26 21:13:26 -0400

    fizz_buzz

---
+++ .history/file_20220326211250.js
@@ -0,0 +1,6 @@
+for (var i = 1; i < 101; i++) { //this will run the numbers from 1 to 101
+    if (i % 15 == 0) console.log("fizzBuzz");// This will divide the number by modules 15
+    else if (i % 3 == 0) console.log("fizz");// This will divide the number by modules 3
+    else if (i % 5 == 0) console.log("buzz");// & this will divide the number by modules 5
+    else console.log(i); //------> Line 
+}
